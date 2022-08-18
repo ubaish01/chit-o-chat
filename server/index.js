93c16d32e180e5c6f75682c98d242bcdef184383
ubaish <ubaish9897@gmail.com>
2022-08-18 23:52:56 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+const path = require("path");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+
+// -------------------------DEPLOYEMENT-----------------------------------------
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==="production")
+{
+  app.use(express.static(path.join(__dirname1,'client/build')));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
+  })
+}
+else 
+{
+  app.get("/",(req,res)=>{
+    res.send("API is running successfully");
+  });
+}
+
+
+
+// -------------------------DEPLOYEMENT-----------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "https://chit-o-chat-app.herokuapp.com/",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
